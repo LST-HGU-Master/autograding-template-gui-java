7632f64b242b58ae5d0cd1f18f79c4d1373c4db3
dai <daihasg@gmail.com>
2022-03-01 15:05:44 +0900

    fix intends

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Not the right string, tests will fail!");
 	}
 
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,25 +1,23 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-
 import java.io.*;
 
 public class HelloTest {
 
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
+    @Test
+    public void testHelloWorld()
+    {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
 
-     // action
-     Hello.main(null);
+        // action
+        Hello.main(null);
 
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
+        // assertion
+        assertEquals("Hello world!\n", bos.toString());
 
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+        // undo the binding in System
+        System.setOut(originalOut);
+    }
 }
